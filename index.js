cebc7ed4e016f4e78b55095a15e7aacfe6b74ccc
Dhruba <93818182+Excaliber917@users.noreply.github.com>
2023-02-12 01:19:23 +0530

    changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var cors = require('cors')
 
 connectTOMongo()
 const app = express()
-const port = 5000
+const PORT = process.env.PORT || 5000
 app.use(cors())
 
 app.use(express.json())
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     // console.log(`Example app listening at http://localhost:${port}`)
     console.log("why are you seeking in console")
 })
